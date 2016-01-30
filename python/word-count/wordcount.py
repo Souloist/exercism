24768613f27a06b8f910b24def88c38fd1b7b0a5
Souloist <rich.shen@nyu.edu>
2016-01-30 14:27:57 -0500

    added gigasecond problem

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -2,13 +2,20 @@ from string import punctuation
 import re
 
 def word_count(string):
-	r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
-	words = r.split(string)
-	count = {}
+	r = re.compile(r'[\s{}]+'.format(re.escape(punctuation))) # filtering out punctuation
+	words = r.split(string) # list of words
+
+	count = {} # dict containing word count
+
 	for word in words:
+
+		# adds word into dict with value 1 if word is not in dict
 		if word.lower() not in count:
 			count[word.lower()] = 1
+		
+		# increments the key value if the word is already a key
 		else:
 			count[word.lower()]+=1
-	count.pop("", None)
+
+	count.pop("", None) # removing empty string key from dict
 	return count
