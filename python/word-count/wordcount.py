28fee49147937c5fb8262d664780a35a20ff7d1d
Souloist <rich.shen@nyu.edu>
2016-01-30 14:06:49 -0500

    added word count solution

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,3 +1,14 @@
-def word_count(strong):
-	pass
-	
+from string import punctuation
+import re
+
+def word_count(string):
+	r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
+	words = r.split(string)
+	count = {}
+	for word in words:
+		if word.lower() not in count:
+			count[word.lower()] = 1
+		else:
+			count[word.lower()]+=1
+	count.pop("", None)
+	return count
---
+++ ruby/hello-world/hello_world.rb
@@ -0,0 +1,8 @@
+class HelloWorld
+	def self.hello(name=0)
+		if name = 0
+			"Hello, World!"
+		end
+		"Hello, " + name + "!"
+	end
+end
